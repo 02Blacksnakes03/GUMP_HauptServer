b5c2387cd26c47f277f0f9114cdb830e91046813
02Blacksnakes03 <blacksnakes2001@gmail.com>
2022-03-18 21:14:02 +0100

    Cars3

---
+++ resources/[GLMP]/GLMP-Cars/names.lua
@@ -0,0 +1,34 @@
+Citizen.CreateThread(function()
+    AddTextEntry("Fahrschule", "Fahrschule")
+    AddTextEntry("super", "Fahrschule")
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry("Staat", "Staat")
+    AddTextEntry("super", "Staat")
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry("Zivi", "Zivi")
+    AddTextEntry("super", "Zivi")
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry("Zivi2", "Zivi2")
+    AddTextEntry("super", "Zivi2")
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry("Zivi3", "Zivi3")
+    AddTextEntry("super", "Zivi3")
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry("Zivi4", "Zivi4")
+    AddTextEntry("super", "Zivi4")
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry("Zivi5", "Zivi5")
+    AddTextEntry("super", "Zivi5")
+end)
