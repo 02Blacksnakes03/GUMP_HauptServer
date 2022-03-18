c0efaf4504a1efb331db1a2c09fe4ddab5d34b33
02Blacksnakes03 <blacksnakes2001@gmail.com>
2022-03-18 21:38:11 +0100

    Scripts

---
+++ resources/interact-sound/fxmanifest.lua
@@ -0,0 +1,19 @@
+-- FXVersion Version
+fx_version 'adamant'
+games {"rdr3","gta5"}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+-- Client Scripts
+client_script 'client/main.lua'
+
+-- Server Scripts
+server_script 'server/main.lua'
+
+-- NUI Default Page
+ui_page "client/html/index.html"
+
+
+files {
+    'client/html/index.html',
+    'client/html/sounds/ring2.ogg'
+}
